0bbf1795890cc1de45daebba4e6a206410fdc1d9
satishs <satish.fnu@gmail.com>
2010-10-01 21:29:00 -0700

    engagevia text change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_engagevia_session',
+  :key         => '_doosracricket_session',
   :secret      => 'c6503c413c79fe59abbb90c552c48968aa6d27fa85ffd97f3f4143a41b8a5c16aadafe9cb9c1b8b59fd20a137642bec1260037869fd2c444d662b00146567e03'
 }
 
