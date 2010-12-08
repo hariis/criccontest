c3a5af1c8cd2f80de9aa78a07f1b1a61771882f8
hariis <hrajagopal@yahoo.com>
2010-12-07 22:21:43 -0800

    put tinymce back

--- public/javascripts/tiny_mce_init.js
+++ public/javascripts/tiny_mce_init.js
@@ -7,15 +7,15 @@ var tinymce_advanced_options = {
   skin_variant : "black",
   theme_advanced_resizing_min_width : 500,
   theme_advanced_resizing_max_width : 800,
-  plugins : "autoresize,paste,safari,table,paste",
+  plugins : "autoresize,paste,safari",
 
   width: "660",
   button_title_map: false,
   apply_source_formatting: true,
   theme_advanced_toolbar_align: "left",
 
-  theme_advanced_buttons1: "formatselect,outdent,indent,seperator,undo,redo",
-  theme_advanced_buttons2: "justifyleft,justifycenter,justifyright,separator,bold,italic,separator,bullist,numlist,link",
+  theme_advanced_buttons1: "",
+  theme_advanced_buttons2: "",
   theme_advanced_buttons3: "", //you have to have this to get an empty third row otherwise you get some elements 
   theme_advanced_toolbar_location: "bottom",
   theme_advanced_resizing : true,
