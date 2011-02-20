bec7624ad5d2344b894025a61d48c5a9581f3e05
satishs <satish.fnu@gmail.com>
2011-02-20 07:40:44 -0800

    display the predictions of other all the time

--- app/controllers/predicitions_controller.rb
+++ app/controllers/predicitions_controller.rb
@@ -101,9 +101,10 @@ class PredicitionsController < ApplicationController
       end
     end 
 
-     if !@match.check_if_match_started
-        predicition_details = "Prediction will be displayed once the match begin"
-     end
+     #Overide the prediction text to just display the text below in email notification.
+     #if !@match.check_if_match_started
+     #   predicition_details = "Prediction will be displayed once the match begin"
+     #end
     
     if prediction_count == 6 && predicition_details.size > 0
         Predicition.predicition_notification(@engagement.post_id, @match, @user_name, predicition_details)
